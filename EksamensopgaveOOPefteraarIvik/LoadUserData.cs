1036ec374db568bae4c1f8dbe14dc0851f05df39
Ivikhostrup <56341364+Ivikhostrup@users.noreply.github.com>
2020-11-24 02:13:24 +0100

    Dataparser begun

---
+++ EksamensopgaveOOPefteraarIvik/LoadUserData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using EksamensopgaveOOPefteraarIvik.Users;
+
+namespace EksamensopgaveOOPefteraarIvik
+{
+    public class LoadUserData
+    {
+        private List<UsersParser> usersList;
+
+        public List<IUser> LoadData()
+        {
+            StreamReader reader = new StreamReader("path");
+            UsersParser parsedUser = new UsersParser(';', );
+            
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                
+            }
+        }
+    }
+}
--- EksamensopgaveOOPefteraarIvik/UsersParser.cs
+++ EksamensopgaveOOPefteraarIvik/UsersParser.cs
@@ -1,7 +1,32 @@
-﻿namespace EksamensopgaveOOPefteraarIvik
+﻿using System;
+using System.Linq.Expressions;
+
+namespace EksamensopgaveOOPefteraarIvik
 {
     public class UsersParser
     {
+        public uint Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public string Email { get; set; }
+
+        public UsersParser(char separator, string csvLine)
+        {
+            string[] fields = csvLine.Split(separator);
+
+            Id = uint.Parse(fields[0]);
+            FirstName = fields[1];
+            LastName = fields[2];
+            Username = fields[3];
+            Balance = decimal.Parse(fields[4]);
+            Email = fields[5];
+        }
         
     }
 }
